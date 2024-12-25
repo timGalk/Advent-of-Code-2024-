8a3c848073d4b407287639aa01d64a6b0ee220c9
timGalk <tivron@outlook.com>
2024-12-25 11:29:54 +0100

    Added part B for day09

--- src/main/kotlin/day09/day09.kt
+++ src/main/kotlin/day09/day09.kt
@@ -1,10 +1,12 @@
 package day09
 
 import Reader
+import java.util.*
 
 val reader = Reader()
-val input = reader.readString("src/main/resources/day09.txt").toList()
-var transformed = transformToBlocks(input.map { it.toString().toInt() })
+val input = (reader.readString("src/main/resources/day09.txt").toList())
+    .map { it.toString().toInt() }
+var transformed = transformToBlocks(input)
 
 
 fun part01 (){
@@ -21,8 +23,67 @@ fun part01 (){
 
 }
 fun part02() {
-
+    val disc = input.toMutableList()
+    println(getPart2Result(disc))
 }
+
+fun getPart2Result(disc: MutableList<Int>): Long {
+    data class FileSection(var id: Int, var firstIndex: Int, var length: Int)
+
+    val fileSections = mutableListOf<FileSection>()
+    val gapIndexMap = mutableMapOf<Int, PriorityQueue<Int>>()
+    (1..9).forEach { gapIndexMap[it] = PriorityQueue() }
+
+    var fileId = 0
+    var isFile = true
+    var currentIndex = 0
+    for (sectionLength in disc) {
+        if (isFile) {
+            fileSections.add(FileSection(fileId, currentIndex, sectionLength))
+            fileId++
+        } else if (sectionLength != 0) {
+            gapIndexMap[sectionLength]!!.add(currentIndex)
+        }
+        currentIndex += sectionLength
+        isFile = !isFile
+    }
+
+    while (fileId > 0) {
+        fileId--
+        val fileIndex = fileSections[fileId].firstIndex
+        val fileLength = fileSections[fileId].length
+
+        val bestGap = gapIndexMap
+            .filter { entry -> entry.key >= fileLength }
+            .filter { entry -> entry.value.isNotEmpty() }
+            .map { entry -> entry.key to entry.value }
+            .minByOrNull { entry -> entry.second.peek() }
+
+        if (bestGap == null) continue
+        val gapLength = bestGap.first
+        val gapIndex = bestGap.second.peek()
+        if (gapIndex > fileIndex) continue
+        bestGap.second.poll()
+
+        fileSections[fileId].apply { firstIndex = gapIndex }
+
+        val remainingGap = gapLength - fileLength
+        if (remainingGap > 0) {
+            val newGapIndex = gapIndex + fileLength
+            gapIndexMap[remainingGap]!!.add(newGapIndex)
+        }
+    }
+
+    return fileSections.sumOf { f ->
+        (0 until f.length).sumOf { index ->
+            (f.id.toLong() * (f.firstIndex + index))
+        }
+    }
+}
+
+
+
+
 
 fun transformToBlocks (list: List<Int>) :List<Int>{
     // let . in string will be a -1
